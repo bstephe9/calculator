52e01e0f77169936eff782ff8060982a7ceb5107
bstephe9 <bstephenson9807@gmail.com>
2025-03-24 18:30:29 -0400

    feat: Add operation function

--- calculator.js
+++ calculator.js
@@ -2,3 +2,17 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+let operand1 = "";
+let operand2 = "";
+let operation = "";
+
+function operate(x, y, operation) {
+  switch (operation) {
+    case "+": return add(x, y);
+    case "-": return subtract(x, y);
+    case "*": return multiply(x, y);
+    case "/": return divide(x, y);
+    default: break;
+  }
+};
